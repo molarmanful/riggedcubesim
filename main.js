47b6e66dd4c161ad48f67c146983c47454114159
Ben Pang <benjinpang@gmail.com>
2015-10-01 20:58:07 -0700

    fix moz bug w/ removeeventlistener

--- main.js
+++ main.js
@@ -12,40 +12,42 @@ function start() {
 	var started = false,
 		timer, move;
 	cube.addEventListener('onShuffleComplete', function () {
-		cube.addEventListener('onTwistComplete', function () {
-			if (!started) {
-				started = true;
-				removeEventListener('onTwistComplete');
-				var startingtime = new Date();
-				timer = setInterval(function () {
-					$('.time').text((new Date - startingtime) / 1000);
-					if (cube.isSolved()) {
-						clearInterval(timer);
-						cube.showStickers();
-						cube.mouseControlsEnabled = true;
-						$('g-cube').attr('class', '');
-						$('.modal').modal('show');
-					} else {
-						var tricks = [
-								'', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
-								'cube.hideStickers()',
-								'cube.showStickers()',
-								'$("g-cube").attr("class",["transparent","white","yellow","green","blue","orange","red","neon"][Math.floor(Math.random()*9)])',
-								'cube.mouseControlsEnabled=false',
-								'cube.mouseControlsEnabled=true'
-							];
-						eval(tricks[Math.floor(Math.random() * tricks.length)]);
-						if (Math.floor(Math.random() * 1000000) == 1) {
-							var moves = ["r", "u", "l", "b", "d", "f", "m", "e", "s", "x", "y", "z"],
-								twist = moves[Math.floor(Math.random() * moves.length)];
-							if (Math.round(Math.random())) {
-								twist = twist.toUpperCase();
-							}
-							cube.twist(twist);
-						}
+		cube.addEventListener('onTwistComplete', timing());
+	});
+}
+
+function timing() {
+	if (!started) {
+		started = true;
+		removeEventListener('onTwistComplete', timing());
+		var startingtime = new Date();
+		timer = setInterval(function () {
+			$('.time').text((new Date - startingtime) / 1000);
+			if (cube.isSolved()) {
+				clearInterval(timer);
+				cube.showStickers();
+				cube.mouseControlsEnabled = true;
+				$('g-cube').attr('class', '');
+				$('.modal').modal('show');
+			} else {
+				var tricks = [
+						'', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+						'cube.hideStickers()',
+						'cube.showStickers()',
+						'$("g-cube").attr("class",["transparent","white","yellow","green","blue","orange","red","neon"][Math.floor(Math.random()*9)])',
+						'cube.mouseControlsEnabled=false',
+						'cube.mouseControlsEnabled=true'
+					];
+				eval(tricks[Math.floor(Math.random() * tricks.length)]);
+				if (Math.floor(Math.random() * 1000000) == 1) {
+					var moves = ["r", "u", "l", "b", "d", "f", "m", "e", "s", "x", "y", "z"],
+						twist = moves[Math.floor(Math.random() * moves.length)];
+					if (Math.round(Math.random())) {
+						twist = twist.toUpperCase();
 					}
-				});
+					cube.twist(twist);
+				}
 			}
 		});
-	});
+	}
 }
